bef7e66238122adcf8290d99a997c774d3cdcd98
jia.li <ga402159806@gmail.com>
2018-12-18 14:22:50 +0800

    添加注释，去除无用代码

--- app/src/main/java/com/gavinandre/aidldemo/BookManagerService.kt
+++ app/src/main/java/com/gavinandre/aidldemo/BookManagerService.kt
@@ -9,20 +9,18 @@ import com.gavinandre.aidl.Book
 import com.gavinandre.aidl.IBookManager
 import com.gavinandre.aidl.IOnNewBookArrivedListener
 import java.util.concurrent.CopyOnWriteArrayList
-import java.util.concurrent.atomic.AtomicBoolean
 
 class BookManagerService : Service() {
 
     private val TAG = BookManagerService::class.java.simpleName
 
-    private val mIsServiceDestroyed = AtomicBoolean(false)
-
+    //使用CopyOnWriteArrayList来支持并发读写
     private val mBookList = CopyOnWriteArrayList<Book>()
+    //使用RemoteCallbackList来支持aidl接口管理
     private val mListenerList = RemoteCallbackList<IOnNewBookArrivedListener>()
 
     override fun onCreate() {
         super.onCreate()
-
     }
 
     override fun onBind(intent: Intent): IBinder? {
@@ -38,38 +36,45 @@ class BookManagerService : Service() {
     }
 
     override fun onDestroy() {
-        mIsServiceDestroyed.set(true)
         super.onDestroy()
     }
 
     private val mBinder = object : IBookManager.Stub() {
         override fun addBook(book: Book) {
+            //模拟耗时操作
             SystemClock.sleep(3000)
+            mBookList.add(book)
+            //通知所有注册监听接口的客户端有新书到达
             onNewBookArrived(book)
         }
 
-        override fun getBookList(): MutableList<Book> {
+        override fun getBookList(): List<Book> {
+            //模拟耗时操作
             SystemClock.sleep(3000)
             return mBookList
         }
 
         override fun registerListener(listener: IOnNewBookArrivedListener?) {
+            //添加aidl接口
             mListenerList.register(listener)
+            //获取aidl接口数量
             val N = mListenerList.beginBroadcast()
             mListenerList.finishBroadcast()
-            Log.d(TAG, "registerListener, current size:$N")
+            Log.i(TAG, "registerListener, current size:$N")
         }
 
         override fun unregisterListener(listener: IOnNewBookArrivedListener?) {
+            //移除aidl接口
             val success = mListenerList.unregister(listener)
             if (success) {
-                Log.d(TAG, "unregister success.")
+                Log.i(TAG, "unregister success.")
             } else {
-                Log.d(TAG, "not found, can not unregister.")
+                Log.i(TAG, "not found, can not unregister.")
             }
+            //获取aidl接口数量
             val N = mListenerList.beginBroadcast()
             mListenerList.finishBroadcast()
-            Log.d(TAG, "unregisterListener, current size:$N")
+            Log.i(TAG, "unregisterListener, current size:$N")
         }
 
         override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
@@ -97,10 +102,10 @@ class BookManagerService : Service() {
     }
 
     fun onNewBookArrived(book: Book) {
-        mBookList.add(book)
         val N = mListenerList.beginBroadcast()
         for (i in 0 until N) {
             try {
+                //通知所有注册监听接口的客户端有新书到达
                 mListenerList
                     .getBroadcastItem(i)
                     ?.onNewBookArrived(book)
